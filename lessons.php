60975be0b98e2e0f2fb2aeac72edd47d4b4c35c4
kda <2repby@gmail.com>
2021-10-23 19:59:33 +0500

    Start

--- lessons.php
+++ lessons.php
@@ -9,28 +9,35 @@
             <th scope="col">Начало</th>
             <th scope="col">Окончание</th>
             <th scope="col">Примечание</th>
+            <th scope="col">Присуствующие</th>
             <?php if(isset($_SESSION['username'])) echo "<th scope='col'>Действие</th>" ?>
         </tr>
         </thead>
         <tbody>
 <?php
 
-$query = $pdo->query('SELECT * FROM lesson,users WHERE users.id = lesson.teacher_id;');
+$query = $pdo->query('SELECT * FROM users,lesson WHERE users.id = lesson.teacher_id;');
 
 while ($row = $query->fetch())
 {
 ?>
 
     <tr>
-            <th scope='row'><?php echo $row['id'] ?></th>
+            <td><?php echo $row['id'] ?></td>
             <td><?php echo $row['name'] ?></td>
             <td><?php echo $row['last_name'].' '.$row['first_name'] ?> </td>
             <td><?php echo $row['student_group'] ?> </td>
             <td><?php echo  $row['start_at'] ?> </td>
             <td><?php echo  $row['end_at'] ?> </td>
             <td><?php echo  $row['comment'] ?> </td>
-
-        </tr>
+            <td>
+                <?php
+                $query2 = $pdo->query('SELECT * FROM users, visit WHERE users.id = visit.id_user AND visit.id_lesson = '.$row['id'].';');
+                while ($row2 = $query2->fetch())
+                    echo $row2['last_name'].' '.$row2['first_name'].'<br>';
+                ?>
+            </td>
+    </tr>
 
 <?php } ?>
         </tbody>
